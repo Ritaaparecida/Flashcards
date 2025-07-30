9a4c75bde56938170475f1d99f20ec2559d787eb
Ritaaparecida <rita.morais@escola.pr.gov.br>
2025-07-30 12:52:59 +0000

    criamos as perguntas no jacsgrifts

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+     "programação",
+    "O que é programação de computadores?",
+    "É o processo de escrever instruções para um computador realizar uma tarefa especifica, usando uma linguagem de programação"
+    )
+
+    criaCartao(
+        "programação",
+        "O que é um algoritmo?",
+        " Uma sequência lógica e finita de passos para resolver um problema."
+    )
+
+    criaCartao(
+         "programação",
+         " Qual a diferença entre programa e software?",
+        " Software é um conjunto de programas e outras instruções que permitem e execução de tarefas, enquanto um programa é uma sequência de instruç"
+        
+    )
