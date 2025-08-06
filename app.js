16d1b5690d0257b2525364a6841e0faddae21f4d
Ritaaparecida <rita.morais@escola.pr.gov.br>
2025-08-06 13:56:37 +0000

    fizemos cartoes javascripts

--- app.js
+++ app.js
@@ -3,6 +3,19 @@
         const cartao = document. createElement('article')
         cartao.className = 'cartao'
 
+        cartao.innerHTML = `<div class="cartao_conteudo">
+                <h3>${categoria}}</h3>
+                <div class="pergunta">
+                    ${pergunta}
+                </div>
+                <div class="resposta">
+                    ${resposta}
+                </div>
+            </div>`
+
+
+
+            container.appendChild(cartao)
+
 
     }
-
