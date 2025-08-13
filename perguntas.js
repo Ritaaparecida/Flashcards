a1b4ea31dc93577c808ed62a2d8e8f2552ca08e7
Ritaaparecida <rita.morais@escola.pr.gov.br>
2025-08-13 12:20:42 +0000

    aaaaaaa

--- perguntas.js
+++ perguntas.js
@@ -13,6 +13,39 @@ criaCartao(
     criaCartao(
          "programação",
          " Qual a diferença entre programa e software?",
-        " Software é um conjunto de programas e outras instruções que permitem e execução de tarefas, enquanto um programa é uma sequência de instruç"
-        
+        " Software é um conjunto de programas e outras instruções que permitem e execução de tarefas, enquanto um programa é uma sequência de instruções escrita em uma linguagem de programação"
+    )
+
+    criaCartao(
+        "programação",
+        "O que é uma linguagem de programação?",
+        "Um conjunto de regras e instruções que permitem a comunicaçaõ com o computador"
+    )
+
+    criaCartao(
+        "programação",
+        "O que são variáveis e tipo de dados?",
+        "Variáveis são locais de armazenamento de dados em um programa, e tipos de dados definem o tipo de informação que pode ser armazenada(números,textos)"
+    )
+
+    criaCartao(
+        "programação",
+        "O que são estruturas de controle(condicional e loops)?",
+        "Estruturas de controle permitem que o programa tome decisões(condicionais) e repita blocos de código(loops)"
+    )
+
+    criaCartao(
+        "programação",
+        "O que é uma função?",
+        "Um bloco de código reutilizável que executa uma tarefa especifica."
+    )
+
+    criaCartao(
+        "programação",
+        "O que é herança?",
+        "Permite que uma classe herde características e comportamentos de outra classes"
+    )
+
+    criaCartao(
+        ""
     )
